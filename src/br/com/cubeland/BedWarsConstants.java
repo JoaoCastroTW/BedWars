a73482d2933477ae0dd2e32f58d7ab8c7bf157b3
João Castro <joao@cubeland.com.br>
2020-10-04 20:13:45 -0300

    Creating constants class.

---
+++ src/br/com/cubeland/BedWarsConstants.java
@@ -0,0 +1,10 @@
+package br.com.cubeland;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class BedWarsConstants {
+
+    public static final Location LOBBY_LOCATION = new Location(Bukkit.getWorld("world"), 0.5, 65, 0.5);
+
+}
