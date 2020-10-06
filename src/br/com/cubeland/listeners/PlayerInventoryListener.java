0b086cb3f8503d0390f1751f737bc794467a4499
João Castro <joao@cubeland.com.br>
2020-10-06 13:22:10 -0300

    Blocking players from picking up beds.

--- src/br/com/cubeland/listeners/PlayerInventoryListener.java
+++ src/br/com/cubeland/listeners/PlayerInventoryListener.java
@@ -1,12 +1,13 @@
 package br.com.cubeland.listeners;
 
 import br.com.cubeland.utils.BlockedDrops;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 
 public class PlayerInventoryListener implements Listener {
 
@@ -20,6 +21,13 @@ public class PlayerInventoryListener implements Listener {
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         if (BlockedDrops.contains(event.getItemDrop().getItemStack().getType().toString())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        if (event.getItem().getItemStack().getType().equals(Material.BED)) {
             event.setCancelled(true);
         }
     }
