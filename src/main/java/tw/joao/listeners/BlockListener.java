e29c0cd04f63856965d312b9d34f2ae6ceff0de6
João Castro <joao@cubeland.com.br>
2020-10-26 19:11:54 -0300

    Add verifications.

--- src/main/java/tw/joao/listeners/BlockListener.java
+++ src/main/java/tw/joao/listeners/BlockListener.java
@@ -1,5 +1,8 @@
 package tw.joao.listeners;
 
+import tw.joao.GameManager;
+import tw.joao.GameStatus;
+import tw.joao.teams.Team;
 import tw.joao.teams.TeamManager;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -15,7 +18,12 @@ public class BlockListener implements Listener {
         Player player = event.getPlayer();
         Block block = event.getBlock();
 
-        if (block.getType().equals(Material.BED_BLOCK)) {
+        if (!GameManager.getGameStatus().equals(GameStatus.IN_PROGRESS)) {
+            event.setCancelled(true);
+            return;
+        }
+
+        if (block.getType().equals(Material.BED_BLOCK) && Team.isAlive(player)) {
             TeamManager.handleBedBreak(player, block);
         }
     }
