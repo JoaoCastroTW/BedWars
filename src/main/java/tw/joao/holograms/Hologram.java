3d49cee71459e31a01e7af859579eb9b3d3c0adf
João Castro <joao@cubeland.com.br>
2020-10-29 17:27:46 -0300

    Make possible hit/interact through holograms

--- src/main/java/tw/joao/holograms/Hologram.java
+++ src/main/java/tw/joao/holograms/Hologram.java
@@ -1,6 +1,10 @@
 package tw.joao.holograms;
 
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftArmorStand;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 
@@ -23,8 +27,8 @@ public class Hologram {
             location.getChunk().load();
         }
 
-        for (int i = 0; i < textLines.size(); i++) {
-            addLine(textLines.get(i));
+        for (String textLine : textLines) {
+            addLine(textLine);
         }
     }
 
@@ -37,6 +41,13 @@ public class Hologram {
                 hologramLines.get(hologramLines.size() - 1).getLocation().clone().subtract(0, 0.25, 0) :
                 this.location;
         ArmorStand line = (ArmorStand) lineLocation.getWorld().spawnEntity(lineLocation, EntityType.ARMOR_STAND);
+
+        EntityArmorStand nmsArmorStand = ((CraftArmorStand) line).getHandle();
+        NBTTagCompound compound = new NBTTagCompound();
+
+        nmsArmorStand.c(compound);
+        compound.setBoolean("Marker", true);
+        nmsArmorStand.f(compound);
 
         line.setSmall(true);
         line.setGravity(false);
