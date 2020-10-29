92942a8560e09a6fb9a3b54bdd1a92fd80bc76c7
João Castro <joao@cubeland.com.br>
2020-10-29 17:25:43 -0300

    Refactor, fix typo and add IslandGenerator hologram

--- src/main/java/tw/joao/messages/HologramMessages.java
+++ src/main/java/tw/joao/messages/HologramMessages.java
@@ -3,7 +3,8 @@ package tw.joao.messages;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import tw.joao.BedWars;
-import tw.joao.generators.NaturalGenerator;
+import tw.joao.generator.island.IslandGenerator;
+import tw.joao.generator.natural.NaturalGenerator;
 import tw.joao.holograms.Hologram;
 
 import static tw.joao.utils.MessageUtils.*;
@@ -13,7 +14,7 @@ public class HologramMessages {
         String[] textLines = {
             translateColorCodes(String.format("&eNível %s", generator.getLevel())),
             translateColorCodes(String.format("&eGerador de %s%s", generator.getColor(), generator.getName())),
-            translateColorCodes(String.format("&eNovos minerios em &a%s &esegundos", generator.getDelay() / 20))
+            translateColorCodes(String.format("&eNovos minerios em &a%s &esegundos", generator.getCurrentCooldown() / 20))
         };
 
         Hologram hologram = new Hologram(generator.getLocation().clone().add(0.5, 2.5, 0.5), textLines);
@@ -22,11 +23,27 @@ public class HologramMessages {
             public void run() {
                 hologram.modifyText(0, translateColorCodes(String.format("&eNível &a%s", generator.getLevel())));
 
-                if (generator.getCurrentCooldown() < 0) {
-                    hologram.modifyText(2, translateColorCodes(String.format("&cExistem muitos minerios por perto")));
+                if (generator.isActive()) {
+                    hologram.modifyText(2, translateColorCodes(String.format("&eNovos minerios em &a%s &esegundos", generator.getCurrentCooldown() / 20)));
                 } else {
-                    hologram.modifyText(2, translateColorCodes(String.format("&eNovos minerios em &a%s &esegundos", generator.getCurrentCooldown())));
+                    hologram.modifyText(2, translateColorCodes("&cExistem muitos minérios por perto"));
                 }
+            }
+        }.runTaskTimer(JavaPlugin.getPlugin(BedWars.class), 0, 20);
+    }
+
+    public static void spawnIslandGeneratorHologram(IslandGenerator generator) {
+        String[] textLines = {
+                translateColorCodes(String.format("&eNível &a%s", generator.getLevel())),
+                translateColorCodes(String.format("&eGerador do time %s%s", generator.getTeam().getColorCode(), generator.getTeam().getName()))
+        };
+
+        Hologram hologram = new Hologram(generator.getLocation().clone().add(0.5, 2.5, 0.5), textLines);
+
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                hologram.modifyText(0, translateColorCodes(String.format("&eNível &a%s", generator.getLevel())));
             }
         }.runTaskTimer(JavaPlugin.getPlugin(BedWars.class), 0, 20);
     }
