b33bc571ebac1b48517f6e2afde6c8c8d2d464ad
João Castro <joao@cubeland.com.br>
2020-10-29 17:18:01 -0300

    Add Lombok and generatorLocation

--- src/main/java/tw/joao/teams/Teams.java
+++ src/main/java/tw/joao/teams/Teams.java
@@ -1,24 +1,26 @@
 package tw.joao.teams;
 
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public enum Teams {
-    RED("Vermelho", "&c", 14, 206, 39, 28, new Location(Bukkit.getWorld("world"), -33.5, 66, 0.5), new Location(Bukkit.getWorld("world"), -30, 66, 0)),
-    GREEN("Verde", "&a", 5, 4, 206, 28, new Location(Bukkit.getWorld("world"), 0.5, 66, -33.5), new Location(Bukkit.getWorld("world"), 0, 66, -30)),
-    BLUE("Azul", "&9", 3, 25, 12, 199, new Location(Bukkit.getWorld("world"), 34.5, 66, 0.5), new Location(Bukkit.getWorld("world"), 31, 66, 0)),
-    ORANGE("Laranja", "&6", 1, 255, 131, 28, new Location(Bukkit.getWorld("world"), 0.5, 66, 34.5), new Location(Bukkit.getWorld("world"), 0, 66, 31));
+    RED("Vermelho", "&c", 14, 206, 39, 28, new Location(Bukkit.getWorld("world"), -33.5, 66, 0.5), new Location(Bukkit.getWorld("world"), -30, 66, 0), new Location(Bukkit.getWorld("world"), -36, 66, 0)),
+    GREEN("Verde", "&a", 5, 4, 206, 28, new Location(Bukkit.getWorld("world"), 0.5, 66, -33.5), new Location(Bukkit.getWorld("world"), 0, 66, -30), new Location(Bukkit.getWorld("world"), 0, 66, -36)),
+    BLUE("Azul", "&9", 3, 25, 12, 199, new Location(Bukkit.getWorld("world"), 34.5, 66, 0.5), new Location(Bukkit.getWorld("world"), 31, 66, 0), new Location(Bukkit.getWorld("world"), 37, 66, 0)),
+    ORANGE("Laranja", "&6", 1, 255, 131, 28, new Location(Bukkit.getWorld("world"), 0.5, 66, 34.5), new Location(Bukkit.getWorld("world"), 0, 66, 31), new Location(Bukkit.getWorld("world"), 0, 66, 37));
 
-    private final String name;
-    private final int data;
-    private final String colorCode;
+    @Getter private final String name;
+    @Getter private final int data;
+    @Getter private final String colorCode;
     private final int colorR;
     private final int colorG;
     private final int colorB;
-    private final Location location;
-    private final Location bedLocation;
+    @Getter private final Location location;
+    @Getter private final Location bedLocation;
+    @Getter private final Location generatorLocation;
 
-    Teams(String name, String colorCode, int woolData, int colorR, int colorG, int colorB, Location location, Location bedLocation) {
+    Teams(String name, String colorCode, int woolData, int colorR, int colorG, int colorB, Location location, Location bedLocation, Location generatorLocation) {
         this.name = name;
         this.data = woolData;
         this.colorCode = colorCode;
@@ -27,18 +29,7 @@ public enum Teams {
         this.colorB = colorB;
         this.location = location;
         this.bedLocation = bedLocation;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getData() {
-        return data;
-    }
-
-    public String getColorCode() {
-        return colorCode;
+        this.generatorLocation = generatorLocation;
     }
 
     public int getColor(char id) {
@@ -52,11 +43,5 @@ public enum Teams {
             default:
                 return 0;
         }
-    }
-
-    public Location getLocation() { return location; }
-
-    public Location getBedLocation() {
-        return bedLocation;
     }
 }
